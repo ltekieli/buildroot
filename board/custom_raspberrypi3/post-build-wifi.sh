67d44a86216389bbeb69b0f2b0a8bf458eb83989
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2019-03-06 20:09:55 +0100

    Allow building image with no wifi

--- board/custom_raspberrypi3/post-build-wifi.sh
+++ board/custom_raspberrypi3/post-build-wifi.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-set -u
 set -e
+
+if [ -z "${WPA_SUPP_SSID}" ] || [ -z "${WPA_SUPP_PASS}" ]; then
+    echo "WARNING: wpa_supplicant will not be configured"
+    echo "WARNING: set WPA_SUPP_SSID and WPA_SUPP_PASS"
+    exit 0
+fi
 
 mkdir -p ${TARGET_DIR}/etc/wpa_supplicant
 wpa_passphrase ${WPA_SUPP_SSID} ${WPA_SUPP_PASS} > ${TARGET_DIR}/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
